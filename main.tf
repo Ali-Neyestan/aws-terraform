af92be90bc4b609a74f8a1b4579de2a4035d8cae
Ali-Neyestan <ali.ghanbari90@gmail.com>
2023-05-04 12:48:26 -0700

    creating Security Group for VPC

--- main.tf
+++ main.tf
@@ -46,3 +46,37 @@ resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
 }
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  description = "Allow ssh and 8080 ports inbound"
+  
+  ingress {
+    description = "ssh to VPC"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "8080 to nginx"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  
+  tags = {
+    Name: "${var.env_prefix}-sg"
+  }
+}
