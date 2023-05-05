7006417b6867574ec2f2014e2b1006183c081caa
Ali-Neyestan <ali.ghanbari90@gmail.com>
2023-05-04 21:07:12 -0700

    Creating aws_instance - aws_key_pair - aws_ami

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "instance_type" {}
+variable "pub_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -78,5 +80,48 @@ resource "aws_security_group" "myapp-sg" {
   
   tags = {
     Name: "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = [ "amazon" ]
+
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm-*-x86_64-gp2" ]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "aws_terraform"
+  public_key = "${file(var.pub_key_location)}"
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [ aws_security_group.myapp-sg.id ]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh_key.key_name
+
+tags = {
+  Name = "${var.env_prefix}-server"
   }
 }
